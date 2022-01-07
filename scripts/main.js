06dc6b2a44ace02ab968e9c751696b43c6a985b9
llewrubnala <alanburwell@hks.harvard.edu>
2022-01-06 20:29:48 -0500

    change 2 buttons to 1

--- scripts/main.js
+++ scripts/main.js
@@ -11,21 +11,26 @@ let fox_showing = true
 
  }
 
-function fox_off() {
 
-	document.getElementById('fox').src = ""
-	document.getElementById('fox').alt = ""
+function fox_swap() {
 
-	fox_showing = false
-	
-}
+	if (fox_showing === true) {
 
-function fox_on() {
-	
-	document.getElementById('fox').src = "images/firefox-icon.png"
-	document.getElementById('fox').alt = "The Firefox logo: a flaming fox surrounding the Earth."
-	
-	fox_showing = true
+		document.getElementById('fox').src = ""
+		document.getElementById('fox').alt = ""
+		document.getElementById('fox_butt').innerText = "Show Fox!"
+
+		fox_showing = false
+
+	} else {
+
+		document.getElementById('fox').src = "images/firefox-icon.png"
+		document.getElementById('fox').alt = "The Firefox logo: a flaming fox surrounding the Earth."
+		document.getElementById('fox_butt').innerText = "Hide Fox!"
+
+		fox_showing = true
+
+	}
 
 }
 
