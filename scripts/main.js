fc6cb582a606f147bfe4d2ba0cc4546389afa2d6
llewrubnala <alanburwell@hks.harvard.edu>
2022-01-06 20:12:37 -0500

    add js script file and add html buttons

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+function fox_off() {
+	document.getElementById('fox').src = ""
+	document.getElementById('fox').alt = ""
+
+	
+}
+
+function fox_on() {
+	//document.getElementById("fox").innerHTML = "<img src='newWatermark.png'/>"
+	//document.getElementById("fox").innerHTML = "<img id='fox' src='images/firefox-icon.png' alt='The Firefox logo: a flaming fox surrounding the Earth.'>"
+	document.getElementById('fox').src = "images/firefox-icon.png"
+	document.getElementById('fox').alt = "The Firefox logo: a flaming fox surrounding the Earth."
+	
+}
+
+
+
+
+
