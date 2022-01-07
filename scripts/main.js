b6d8dd3392a87c774abdaf3db890870e5538cf07
llewrubnala <alanburwell@hks.harvard.edu>
2022-01-06 20:13:50 -0500

    add boolean fox tracking in js

--- scripts/main.js
+++ scripts/main.js
@@ -2,21 +2,24 @@
 
 
 
-
+let fox_showing = true
 
 
 function fox_off() {
+
 	document.getElementById('fox').src = ""
 	document.getElementById('fox').alt = ""
 
+	fox_showing = false
 	
 }
 
 function fox_on() {
-	//document.getElementById("fox").innerHTML = "<img src='newWatermark.png'/>"
-	//document.getElementById("fox").innerHTML = "<img id='fox' src='images/firefox-icon.png' alt='The Firefox logo: a flaming fox surrounding the Earth.'>"
+	
 	document.getElementById('fox').src = "images/firefox-icon.png"
 	document.getElementById('fox').alt = "The Firefox logo: a flaming fox surrounding the Earth."
+	
+	fox_showing = true
 	
 }
 
